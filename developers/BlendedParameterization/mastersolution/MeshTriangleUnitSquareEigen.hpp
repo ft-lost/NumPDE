f8f81fe2664921a30d260321c9261a0981eaebff
Bob Schreiner <bobschreiner@student-net-cx-1013.intern.ethz.ch>
2023-07-27 12:21:54 +0200

    Ran clang-tidy on MeshTriangleUnitSquareEigen.hpp

--- developers/BlendedParameterization/mastersolution/MeshTriangleUnitSquareEigen.hpp
+++ developers/BlendedParameterization/mastersolution/MeshTriangleUnitSquareEigen.hpp
@@ -4,30 +4,32 @@
 
 #include <Eigen/Dense>
 
-Eigen::MatrixXd generateMesh(int M)
-{
-    double h = 1.0/(M+1);
-    
-    Eigen::MatrixXd nodes((M+2)*(M+2), 2);
-    for(int i=0; i<M+2; ++i) {
-        for(int j=0; j<M+2; ++j) {
-            nodes(i*(M+2)+j, 0) = i*h;
-            nodes(i*(M+2)+j, 1) = j*h;
-        }
-    }
+Eigen::MatrixXd generateMesh(int M) {
+  double h = 1.0 / (M + 1);
 
-    Eigen::MatrixXd elements(2*(M+1)*(M+1), 6);
-    int cnt = 0;
-    for(int i=0; i<M+1; ++i) {
-        for(int j=0; j<M+1; ++j) {
-            elements.row(cnt) << nodes.row(i*(M+2) + j), nodes.row((i+1)*(M+2) + j),   nodes.row((i+1)*(M+2) + j+1);
-            ++cnt;
-            elements.row(cnt) << nodes.row(i*(M+2) + j), nodes.row((i+1)*(M+2) + j+1), nodes.row(i*(M+2) + j+1);
-            ++cnt;
-        }
+  Eigen::MatrixXd nodes((M + 2) * (M + 2), 2);
+  for (int i = 0; i < M + 2; ++i) {
+    for (int j = 0; j < M + 2; ++j) {
+      nodes(i * (M + 2) + j, 0) = i * h;
+      nodes(i * (M + 2) + j, 1) = j * h;
     }
+  }
 
-    return elements;
+  Eigen::MatrixXd elements(2 * (M + 1) * (M + 1), 6);
+  int cnt = 0;
+  for (int i = 0; i < M + 1; ++i) {
+    for (int j = 0; j < M + 1; ++j) {
+      elements.row(cnt) << nodes.row(i * (M + 2) + j),
+          nodes.row((i + 1) * (M + 2) + j),
+          nodes.row((i + 1) * (M + 2) + j + 1);
+      ++cnt;
+      elements.row(cnt) << nodes.row(i * (M + 2) + j),
+          nodes.row((i + 1) * (M + 2) + j + 1), nodes.row(i * (M + 2) + j + 1);
+      ++cnt;
+    }
+  }
+
+  return elements;
 }
 
-#endif // MESH_TRIANGLE_UNIT_SQUARE_EIGEN
+#endif  // MESH_TRIANGLE_UNIT_SQUARE_EIGEN
