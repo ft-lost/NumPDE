1eeb38c3f383292d58f04a543a1322530bf47116
Raffael Casagrande <raffael@casagrande.ch>
2024-02-01 18:15:43 +0100

    fix narrow.h + multiple definitions of boost

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,8 +1,9 @@
 # This file specifies additional data for the dependencies that are imported via hunter
 
 hunter_config(lehrfempp
-  URL "https://github.com/craffael/lehrfempp/archive/release-0.9.0.tar.gz"
-  SHA1 "825ac9dd9de8e7e86564ce01da4d1423cc54ccbf"
+  URL "https://github.com/craffael/lehrfempp/archive/release-0.9.1.tar.gz"
+  SHA1 "be5e21a0d9cbac7291f09b281b7392a42f611284"
   CMAKE_ARGS LF_REDIRECT_ASSERTS=Off
 )
 hunter_config(Eigen VERSION 3.4.0)
+hunter_config(Boost VERSION 1.78.0)
