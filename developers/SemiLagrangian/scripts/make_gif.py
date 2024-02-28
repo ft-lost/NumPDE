846ed35a3aea7085c58ea42ace84f827d721bd65
msaladin <msaladin@ethz.ch>
2024-02-28 22:03:42 +0100

    avoid using imageio as it isn't included on runners by default

--- developers/SemiLagrangian/scripts/make_gif.py
+++ developers/SemiLagrangian/scripts/make_gif.py
@@ -1,21 +1,8 @@
 import matplotlib.pyplot as plt
-import imageio
+import matplotlib.animation as animation
 from sys import argv
 import numpy as np
 from numpy import genfromtxt
-
-# function that will create a single snapshot at time t
-def create_frame(x, y , solution, t):
-    fig = plt.figure()
-    ax = plt.axes(projection="3d")
-    ax.set_xlim3d(left=0 , right=1)
-    ax.set_ylim3d(bottom=0 , top=1)
-    ax.set_zlim3d(bottom=0 , top=1)
-    ax.plot_surface(x, y , solution , cmap='viridis')
-    ax.set_title("Transport at {} seconds".format(t))
-    plt.savefig(f"img/img_{t}.eps", transparent=False, facecolor="white" , format="eps")
-    plt.close()
-
 
 input_file = str(argv[1])
 M = int(argv[2])
@@ -27,18 +14,20 @@ x = np.linspace(0,1,M-1)
 y = np.linspace(0,1,M-1)
 
 X,Y = np.meshgrid(x,y)
-# Create all the frames
-for i, t in enumerate(time_steps):
-    create_frame(X, Y, data[i].reshape((M-1,M-1)), t)
 
-# Gather all the frames
-frames = []
-for t in time_steps:
-    image = imageio.v2.imread(f'img/img_{t}.eps')
-    frames.append(image)
-# Now we create the final gif
-imageio.mimsave('solution.gif', # output gif
-                frames,          # array of input frames
-                fps=60)          # optional: frames per second
+fig = plt.figure()
+ax = plt.axes(projection="3d")
+
+def update(i):
+    ax.clear()
+    ax.set_xlim3d(left=0 , right=1)
+    ax.set_ylim3d(bottom=0 , top=1)
+    ax.set_zlim3d(bottom=0 , top=1)
+    ax.plot_surface(X, Y, data[i].reshape((M-1,M-1)), cmap='viridis')
+    ax.set_title("Transport at {} seconds".format(time_steps[i]))
+
+ani = animation.FuncAnimation(fig, update, frames=len(time_steps), interval=1000/60)
+
+ani.save('solution.gif', writer='imagemagick', fps=60)
 
 print("Generated solution.gif")
