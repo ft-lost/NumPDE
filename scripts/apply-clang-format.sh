d0cdc95290e34e92d5876ae2a99aa767a2ed994c
msaladin <msaladin@ethz.ch>
2024-03-05 16:30:59 +0100

    shouldn't apply to homeworks folder

--- scripts/apply-clang-format.sh
+++ scripts/apply-clang-format.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # Very basic script, must be run from the root of the repository (and only on Linux)
-find . -iname *.h -o -iname *.hpp -o -iname *.c -o -iname *.cpp -o -iname *.cc | xargs clang-format -i
+find developers lecturecodes include -iname *.h -o -iname *.hpp -o -iname *.c -o -iname *.cpp -o -iname *.cc | xargs clang-format -i
