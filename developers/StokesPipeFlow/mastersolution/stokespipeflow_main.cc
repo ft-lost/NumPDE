3400c8e9a8cf5585227c72f98190a5093cfd2fe7
Wouter Tonnon <wtonnon@math.ethz.ch>
2024-05-23 14:21:28 +0200

    Added visualisation

--- developers/StokesPipeFlow/mastersolution/stokespipeflow_main.cc
+++ developers/StokesPipeFlow/mastersolution/stokespipeflow_main.cc
@@ -10,32 +10,43 @@
 
 #include "stokespipeflow.h"
 
-int main(int /*argc*/, char** /*argv*/) {
+int main(int /*argc*/, char ** /*argv*/) {
   std::cout << "NumPDE homework problem StokesPipeFlow by Wouter Tonnon (R)\n";
   // Read *.msh_file
   auto mesh_factory = std::make_unique<lf::mesh::hybrid2d::MeshFactory>(2);
   lf::io::GmshReader reader(std::move(mesh_factory), "meshes/pipe.msh");
-  // print all physical entities:
-  std::cout << "Physical Entities in Gmsh File " << std::endl;
-  std::cout
-      << "---------------------------------------------------------------\n";
-  for (lf::base::dim_t codim = 0; codim <= 2; ++codim) {
-    for (auto& pair : reader.PhysicalEntities(codim)) {
-      std::cout << "codim = " << static_cast<int>(codim) << ": " << pair.first
-                << " <=> " << pair.second << std::endl;
-    }
+
+  const std::shared_ptr<const lf::mesh::Mesh> mesh_ptr = reader.mesh();
+  const lf::mesh::Mesh &mesh{*mesh_ptr};
+
+  auto fe_space = std::make_shared<lf::uscalfe::FeSpaceLagrangeO1<double>>(mesh_ptr);
+
+  // Initialize dof handler
+  const lf::assemble::DofHandler &dofh{fe_space->LocGlobMap()};
+
+  const lf::base::size_type N_dofs(dofh.NumDofs());
+
+  lf::io::VtkWriter vtk_writer(mesh_ptr, "test.vtk");
+  auto u1 = lf::mesh::utils::MeshFunctionGlobal([](const Eigen::Vector2d& x) {
+    return -x[1];
+  });  
+  auto u2 = lf::mesh::utils::MeshFunctionGlobal([](const Eigen::Vector2d& x) {
+    return x[0];
+  });
+
+  vtk_writer.WritePointData("u1", u1);
+  vtk_writer.WritePointData("u2", u2);
+  vtk_writer.WritePointData("p", u1);
+ 
+
+/*
+  auto cell_data_ref =
+      lf::mesh::utils::make_CodimMeshDataSet<double>(mesh_p, 0);
+  for (const lf::mesh::Entity *cell : mesh_p->Entities(0)) {
+    int row = dofh.GlobalDofIndices(*cell)[0];
+    cell_data_ref->operator()(*cell) = solution[row];
   }
-  std::cout << std::endl << std::endl;
-
-  // Get Physical Entity Number from it's name (as specified in Gmsh):
-  auto air_nr = reader.PhysicalEntityName2Nr("air");
-
-  // Get all codim=0 entities that belong to the "air" physical entity:
-  for (auto& e : reader.mesh()->Entities(0)) {
-    if (reader.IsPhysicalEntity(*e, air_nr)) {
-      // This entity belongs to the "air" physical entity.
-    }
-  }
-
+  vtk_writer.WriteCellData(name, *cell_data_ref);
+*/
   return 0;
 }
