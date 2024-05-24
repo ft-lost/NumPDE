1ed6905185c688827c00a789e40da7b594a1967d
Wouter Tonnon <wtonnon@math.ethz.ch>
2024-05-24 10:56:29 +0200

    added initial implementation of AllPipeFlow and changed main

--- developers/StokesPipeFlow/mastersolution/stokespipeflow.cc
+++ developers/StokesPipeFlow/mastersolution/stokespipeflow.cc
@@ -147,7 +147,96 @@ lf::assemble::COOMatrix<double> buildTaylorHoodGalerkinMatrix(
 /* SAM_LISTING_END_3 */
 
 double allPipeFlow(PowerFlag powerflag, bool producevtk, const char* meshfile,
-                   const char* outfile) {}
+                   const char* outfile) {
+  const std::string& meshfile_str = std::string(meshfile);
+
+  auto mesh_factory = std::make_unique<lf::mesh::hybrid2d::MeshFactory>(2);
+  lf::io::GmshReader reader(std::move(mesh_factory), meshfile_str);
+
+  const std::shared_ptr<const lf::mesh::Mesh> mesh_ptr = reader.mesh();
+  const lf::mesh::Mesh& mesh{*mesh_ptr};
+
+  // Initialize dof handler for the Stokes solver
+  lf::assemble::UniformFEDofHandler dofh(mesh_ptr,
+                                         {{lf::base::RefEl::kPoint(), 3},
+                                          {lf::base::RefEl::kSegment(), 2},
+                                          {lf::base::RefEl::kTria(), 0},
+                                          {lf::base::RefEl::kQuad(), 0}});
+
+  // Initialize dof handler for the components of the velocity
+  lf::assemble::UniformFEDofHandler dofh_u(mesh_ptr,
+                                           {{lf::base::RefEl::kPoint(), 1},
+                                            {lf::base::RefEl::kSegment(), 1},
+                                            {lf::base::RefEl::kTria(), 0},
+                                            {lf::base::RefEl::kQuad(), 0}});
+
+  // Initialize dof handler for the pressure p
+  lf::assemble::UniformFEDofHandler dofh_p(mesh_ptr,
+                                           {{lf::base::RefEl::kPoint(), 1},
+                                            {lf::base::RefEl::kSegment(), 0},
+                                            {lf::base::RefEl::kTria(), 0},
+                                            {lf::base::RefEl::kQuad(), 0}});
+
+  // We define the right-hand side
+  auto g = [](Eigen::VectorXd x) -> Eigen::VectorXd {
+    Eigen::VectorXd out(2);
+    out(0) = 1.;
+    out(1) = 0.;
+    return out;
+  };
+
+  // Solve the system
+  auto res = StokesPipeFlow::solvePipeFlow(dofh, g);
+
+  // Coefficient vectors for the first and second component of the velocity
+  Eigen::VectorXd coeff_vec_u1(dofh_u.NumDofs());
+  Eigen::VectorXd coeff_vec_u2(dofh_u.NumDofs());
+
+  // Coefficient vector for the pressure
+  Eigen::VectorXd coeff_vec_p(dofh_p.NumDofs());
+
+  // We first loop over vertices, then over edges
+  for (int codim = 2; codim >= 1; codim--) {
+    for (auto e : mesh.Entities(2)) {
+      // Global indices for u1, u2 for the respective vertex or edge
+      auto glob_idxs = dofh.GlobalDofIndices(*e);
+      auto glob_idx_o2 = dofh_u.GlobalDofIndices(*e)[0];
+
+      // Global indices for p for the respective vertex
+      lf::assemble::gdof_idx_t glob_idx_o1 = -1;
+      if (codim == 2) glob_idx_o1 = dofh_p.GlobalDofIndices(*e)[0];
+
+      // Extract the correct elements for the coefficient matrix of the
+      // components of u
+      coeff_vec_u1(glob_idx_o2) = res(glob_idxs[0]);
+      coeff_vec_u2(glob_idx_o2) = res(glob_idxs[1]);
+
+      // The pressure is only defined on vertices
+      if (codim == 2) coeff_vec_p(glob_idx_o1) = res(glob_idxs[2]);
+    }
+  }
+
+  // Define first- and second-order FE spaces
+  auto fes_o1_ptr =
+      std::make_shared<lf::uscalfe::FeSpaceLagrangeO1<double>>(mesh_ptr);
+  auto fes_o2_ptr =
+      std::make_shared<lf::uscalfe::FeSpaceLagrangeO2<double>>(mesh_ptr);
+
+  // Define finite-element mesh functions
+  lf::fe::MeshFunctionFE<double, double> mf_o2_u1(fes_o2_ptr, coeff_vec_u1);
+  lf::fe::MeshFunctionFE<double, double> mf_o2_u2(fes_o2_ptr, coeff_vec_u2);
+  lf::fe::MeshFunctionFE<double, double> mf_o1_p(fes_o1_ptr, coeff_vec_p);
+
+  // lf::fe::fe::MeshFUnctionFE<double,double> mf_o2()
+
+  lf::io::VtkWriter vtk_writer(mesh_ptr, "test.vtk");
+
+  vtk_writer.WritePointData("u1", mf_o2_u1);
+  vtk_writer.WritePointData("u2", mf_o2_u2);
+  vtk_writer.WritePointData("p", mf_o1_p);
+
+  return 0;
+}
 
 void visualizeTHPipeFlow(const char* meshfile, const char* outfile) {
   (void)allPipeFlow(NOCMOP, true, meshfile, outfile);
